b1c72a0a35bc16cdfe31066b6ac8b0332dd57db9
@tshemake <tshemake@gmail.com>
2018-09-13 21:51:57 +0500

    Add Policy

---
+++ Facebook.Messenger.Client/Infrastructure/AsyncHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Facebook.Messenger.Client.Infrastructure
+{
+    public static class AsyncHelper
+    {
+        public static ThreadPoolRedirector RedirectToThreadPool() => new ThreadPoolRedirector();
+
+        public static async Task ProcessCollectionAsync<T>(IEnumerable<T> coll, Func<T, Task> method)
+        {
+            var tasks = coll.Select(method).ToList();
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+        }
+    }
+
+    public struct ThreadPoolRedirector : INotifyCompletion
+    {
+        // awaiter и awaitable в одном флаконе
+        public ThreadPoolRedirector GetAwaiter() => this;
+
+        // true означает выполнять продолжение немедленно 
+        public bool IsCompleted => Thread.CurrentThread.IsThreadPoolThread;
+
+        public void OnCompleted(Action continuation) =>
+            ThreadPool.QueueUserWorkItem(o => continuation());
+
+        public void GetResult() { }
+    }
+}
--- Facebook.Messenger.Client/Infrastructure/FacebookMessengerService.cs
+++ Facebook.Messenger.Client/Infrastructure/FacebookMessengerService.cs
@@ -8,36 +8,51 @@ using Facebook.Messenger.Library.Core.Objects;
 using Facebook.Messenger.Library.Core.WebhookEvents;
 using Flurl;
 using Flurl.Http;
+using Polly;
 
 namespace Facebook.Messenger.Client.Infrastructure
 {
     public class FacebookMessengerService : Agent
     {
+        private static Policy exponentialRetryPolicy = 
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(2, attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)));
+
         public override async Task SendTextMessageAsync(MessageRecievedEvent<MessageResponse> message)
         {
-            await GraphApiUrl
-                .AppendPathSegment("v2.6/me/messages")
-                .SetQueryParams(new { access_token = PAGE_ACCESS_TOKEN })
-                .PostJsonAsync(message);
+            await AsyncHelper.RedirectToThreadPool();
+
+            await exponentialRetryPolicy.ExecuteAsync(() => 
+                GraphApiUrl
+                    .AppendPathSegment("v2.6/me/messages")
+                    .SetQueryParams(new { access_token = PAGE_ACCESS_TOKEN })
+                    .PostJsonAsync(message));
         }
 
         public override async Task<long> MessageCreativesRequestAsync<T>(BroadcastRequest<T> message)
         {
-            var messageCreatives = await GraphApiUrl
-                .AppendPathSegment("v2.6/me/message_creatives")
-                .SetQueryParams(new { access_token = PAGE_ACCESS_TOKEN })
-                .PostJsonAsync(message)
-                .ReceiveJson<BroadcastResponse>();
+            await AsyncHelper.RedirectToThreadPool();
+
+            var messageCreatives = await exponentialRetryPolicy.ExecuteAsync(() =>
+                GraphApiUrl
+                    .AppendPathSegment("v2.6/me/message_creatives")
+                    .SetQueryParams(new { access_token = PAGE_ACCESS_TOKEN })
+                    .PostJsonAsync(message)
+                    .ReceiveJson<BroadcastResponse>());
+
             return messageCreatives.MessageCreativeId;
         }
 
         public override async Task<long> SendBroadcastMessagesAsync(BroadcastMessageRequest message)
         {
-            var messageBroadcast = await GraphApiUrl
-                .AppendPathSegment("v2.6/me/broadcast_messages")
-                .SetQueryParams(new { access_token = PAGE_ACCESS_TOKEN })
-                .PostJsonAsync(message)
-                .ReceiveJson<BroadcastMessageResponse>();
+            var messageBroadcast = await exponentialRetryPolicy.ExecuteAsync(() =>
+                GraphApiUrl
+                    .AppendPathSegment("v2.6/me/broadcast_messages")
+                    .SetQueryParams(new { access_token = PAGE_ACCESS_TOKEN })
+                    .PostJsonAsync(message)
+                    .ReceiveJson<BroadcastMessageResponse>());
+
             return messageBroadcast.BroadcastId;
         }
     }
