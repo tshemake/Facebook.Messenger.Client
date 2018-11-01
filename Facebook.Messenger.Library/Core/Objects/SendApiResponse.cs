c25b2d83a69be58c6c27a77719250a938e016a08
@tshemake <tshemake@gmail.com>
2018-11-01 19:00:30 +0500

    Send API Response

---
+++ Facebook.Messenger.Library/Core/Objects/SendApiResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Facebook.Messenger.Library.Core.Objects
+{
+    public class SendApiResponse
+    {
+        [JsonProperty("recipient_id")]
+        public virtual string RecipientId { get; set; }
+
+        [JsonProperty("message_id")]
+        public virtual string MessageId { get; set; }
+
+        [JsonProperty("attachment_id")]
+        public virtual string AttachmentId { get; set; }
+    }
+}
